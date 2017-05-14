a04e208741462b26998d633a80615c79339f79bb
squeemishly <squeemishly@gmail.com>
2017-05-14 16:50:20 -0600

    delete functionality complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,13 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to ('/articles')
+  end
+
   private
 
   def article_params
